cfc061e2b05bf8cfb0c98b7714a95dd0c7583f5b
Sujal Shresth <sujshrest@example.com>
2024-04-15 17:58:38 +0530

    Designed the DashBoard

--- src/pages/NoteNook.jsx
+++ src/pages/NoteNook.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import Connections from "../components/Connections";
 function NoteNook() {
   return (
-    <div>
+    <div className="noteNookApp fixed top-0 left-0 w-screen h-screen">
       <NavBar />
       <Header />
       <div className="noteNook">
