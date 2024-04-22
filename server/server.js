e634466358d58b66e92361f1c35aaed2ce6e5e29
Sujal Shresth <sujshrest@example.com>
2024-04-22 16:43:24 +0530

    Added min length to title and subject field of the notes

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://main--testingnotenook.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
